69a12f11ca5d1ba7bd9ac63d4ca911427771ba9f
Serhii Ivanchenko <s.ivanchenko.m@gmail.com>
2023-03-05 14:19:25 +0200

    Rendering data menu to html and style this layout

--- data.js
+++ data.js
@@ -21,3 +21,4 @@ export const menuArray = [
     id: 2,
   },
 ];
+
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import { menuArray } from './data.js';
-// console.log(menuArray);
+const menuDiv = document.getElementById('menu');
+
+menuArray.forEach((menuItem) => {
+  const menuItemDiv = document.createElement('div');
+  menuItemDiv.classList.add('menu__item')
+  menuItemDiv.innerHTML = `
+    <div class="menu__item-emoji">${menuItem.emoji}</div>
+    <div class="menu__item-description">
+      <h4 class="menu__item-name">${menuItem.name}</h4>
+      <p class="menu__item-ingredients">${menuItem.ingredients.join(', ')}</p>
+      <p class="menu__item-price">$${menuItem.price}</p>
+    </div>
+    <div class="menu__item-circle">
+      <button id="order" class="menu__item-plus">+</button>
+    </div>`;
+  menuDiv.appendChild(menuItemDiv);
+});
 
